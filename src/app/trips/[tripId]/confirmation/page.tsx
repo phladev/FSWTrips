346b0da0cf60b2bfce71e64c2a68982e48ec0c79
pxdrobdf <phladev05@gmail.com>
2023-07-08 11:46:28 -0300

    feat: add auth validation to trip confirmation

---
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import Button from "@/components/Button";
+import { Trip } from "@prisma/client";
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import ReactCountryFlag from "react-country-flag";
+
+function TripConfirmation({ params }: { params: { tripId: string } }) {
+  const [trip, setTrip] = useState<Trip | null>();
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  const { status } = useSession();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const fetchTrip = async () => {
+      const response = await fetch(`/api/trips/check`, {
+        method: "POST",
+        body: JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get("startDate"),
+          endDate: searchParams.get("endDate"),
+        }),
+      });
+
+      const res = await response.json();
+
+      setTrip(res.trip);
+      setTotalPrice(res.totalPrice);
+    };
+
+    if (status === "unauthenticated") {
+      router.push("/");
+    }
+
+    fetchTrip();
+  }, [status]);
+
+  if (!trip) return null;
+
+  const startDate = new Date(searchParams.get("startDate") as string);
+  const endDate = new Date(searchParams.get("endDate") as string);
+  const guests = searchParams.get("guests");
+
+  return (
+    <div className="container mx-auto p-5">
+      <h1 className="font-semibold text-xl text-primaryDarker ">Sua viagem</h1>
+
+      {/* CARD */}
+      <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
+        <div className="flex items-center gap-3 pb-5 border-b border-l-grayLighter border-solid">
+          <div className="relative h-[106px] w-[124px]">
+            <Image
+              src={trip.coverImage}
+              fill
+              style={{ objectFit: "cover" }}
+              alt={trip.name}
+              className="rounded-lg"
+            />
+          </div>
+          <div className="flex flex-col">
+            <h2 className="text-lg text-primaryDarker font-semibold">
+              {trip.name}
+            </h2>
+            <div className="flex items-center gap-1">
+              <ReactCountryFlag countryCode={trip.countryCode} svg />
+              <p className="text-xs text-grayPrimary underline">
+                {trip.location}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <h3 className="font-semibold text-lg text-primaryDarker mt-3">
+          Informações sobre o preço
+        </h3>
+        <div className="flex justify-between mt-2">
+          <p className="text-primaryDarker">Total</p>
+          <p className="font-medium">R${totalPrice}</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-5 text-primaryDarker">
+        <h3 className="font-semibold">Data</h3>
+        <div className="flex items-center gap-1 mt-1">
+          <p>{format(startDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+          {" - "}
+          <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+        </div>
+
+        <h3 className="font-semibold mt-5">Hóspedes</h3>
+        <p>{guests} hóspedes</p>
+
+        <Button className="mt-5" variant={"primary"} onClick={() => {}}>
+          Finalizar Compra
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default TripConfirmation;
