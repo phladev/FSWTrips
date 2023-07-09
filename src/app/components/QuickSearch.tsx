897810cc95cfc454b1b901e6fad45f129e3f3baa
pxdrobdf <phladev05@gmail.com>
2023-07-09 11:31:35 -0300

    feat: update quick search

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -17,32 +17,29 @@ const QuickSearch = () => {
         <div className="flex flex-col items-center gap-1">
           <Link href={`/trips/search?text=hotel`}>
             <Image width={35} height={35} src="/hotel-icon.png" alt="Hotel" />
+            <p className="text-sm text-grayPrimary">Hotel</p>
           </Link>
-
-          <p className="text-sm text-grayPrimary">Hotel</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
           <Link href={`/trips/search?text=fazenda`}>
             <Image width={35} height={35} src="/farm-icon.png" alt="Fazenda" />
+            <p className="text-sm text-grayPrimary">Fazenda</p>
           </Link>
-
-          <p className="text-sm text-grayPrimary">Fazenda</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
           <Link href={`/trips/search?text=chalé`}>
             <Image width={35} height={35} src="/cottage-icon.png" alt="Chalé" />
+            <p className="text-sm text-grayPrimary">Chalé</p>
           </Link>
-          <p className="text-sm text-grayPrimary">Chalé</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
           <Link href={`/trips/search?text=pousada`}>
             <Image width={35} height={35} src="/inn-icon.png" alt="Pousada" />
+            <p className="text-sm text-grayPrimary">Pousada</p>
           </Link>
-
-          <p className="text-sm text-grayPrimary">Pousada</p>
         </div>
       </div>
     </div>
